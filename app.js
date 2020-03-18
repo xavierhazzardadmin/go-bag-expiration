d1294a79c5f8c6f8c4b3eb395d85b8cbfaa861cf
xavierhazzardadmin <xavier@hazzard.org.uk>
2020-03-18 12:58:31 +0000

    Added items list, and expiration date to app

--- app.js
+++ app.js
@@ -2,9 +2,22 @@
 var timer = document.getElementById("timer");
 var toggleStartApp = document.getElementById("openSearch");
 var timeLeft = document.getElementById("timeLeft");
+var expirationDate = document.getElementById("expirationDate");
+
 //Global variables
 var time;
 var active = false;
+var message = "This item runs out on: ";
+
+//global functions
+function printExpDate(index) {
+  $("#expirationDate").css("visibility", "visible");
+  var arrayToString = dates[index].toString();
+  expirationDate.innerHTML = message + arrayToString;
+}
+
+//Arrays
+var dates = ["Jan 5, 2021", "Jan 1, 2022"];
 
 const startApp = () => {
   active = true;
@@ -12,6 +25,8 @@ const startApp = () => {
   switch (userInput) {
     case "cous cous":
       // clearInterval(x);
+      $("#list").css("display", "none");
+      $("#timeLeft").css("visibility", "visible");
 
       console.log("This function works");
       // Update the count down every 1 second
@@ -39,17 +54,24 @@ const startApp = () => {
         timer.innerHTML =
           days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
+        //hide the list div
+
         // If the count down is finished, write some text
         if (distance < 0) {
           clearInterval(time);
           timer.innerHTML = "EXPIRED";
         }
+
+        printExpDate(0);
       }, 1000);
 
-      $("#timeLeft").css("visibility", "visible");
       break;
     case "cous":
       // clearInterval(x);
+
+      //hide the list div
+      $("#list").css("display", "none");
+      $("#timeLeft").css("visibility", "visible");
 
       console.log("This function works");
       // Update the count down every 1 second
@@ -82,11 +104,14 @@ const startApp = () => {
           clearInterval(time);
           timer.innerHTML = "EXPIRED";
         }
+        printExpDate(0);
       }, 1000);
 
-      $("#timeLeft").css("visibility", "visible");
       break;
     case "first aid kit":
+      //hide the list div
+      $("#list").css("display", "none");
+      $("#timeLeft").css("visibility", "visible");
       //Change the time left text
       timeLeft.innerText = `The time remaining for ${userInput} is:`;
       // clearInterval(x);
@@ -118,11 +143,14 @@ const startApp = () => {
           clearInterval(time);
           timer.innerHTML = "EXPIRED";
         }
+        printExpDate(1);
       }, 1000);
 
-      $("#timeLeft").css("visibility", "visible");
       break;
     case "first aid":
+      //hide the list div
+      $("#list").css("display", "none");
+      $("#timeLeft").css("visibility", "visible");
       //Change the time left text
       timeLeft.innerText = `The time remaining for first aid kit is:`;
       // clearInterval(x);
@@ -154,11 +182,14 @@ const startApp = () => {
           clearInterval(time);
           timer.innerHTML = "EXPIRED";
         }
+        printExpDate(1);
       }, 1000);
 
-      $("#timeLeft").css("visibility", "visible");
       break;
     case "first aid box":
+      //hide the list div
+      $("#list").css("display", "none");
+      $("#timeLeft").css("visibility", "visible");
       //Change the time left text
       timeLeft.innerText = `The time remaining for first aid kit is:`;
       // clearInterval(x);
@@ -190,9 +221,9 @@ const startApp = () => {
           clearInterval(time);
           timer.innerHTML = "EXPIRED";
         }
+        printExpDate(1);
       }, 1000);
 
-      $("#timeLeft").css("visibility", "visible");
       break;
     default:
       console.log("The item you have search, doesn't exist!");
